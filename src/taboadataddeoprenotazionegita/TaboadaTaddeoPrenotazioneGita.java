c0662579150981f7f71eb111023e21a4198a9563
TaboadaTaddeo22 <taboada.taddeo@franchettisalviani.net>
2026-03-03 11:48:39 +0100

    Initial commit

---
+++ src/taboadataddeoprenotazionegita/TaboadaTaddeoPrenotazioneGita.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taboadataddeoprenotazionegita;
+
+/**
+ *
+ * @author taboada.taddeo
+ */
+public class TaboadaTaddeoPrenotazioneGita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FrameGita fG = new FrameGita();
+        fG.setLocationRelativeTo(null);
+        fG.setVisible(true);
+    }
+    
+}
